e670f105865b5b118a6269e3c4c0e925cb7178ab
vlkrrrr <yakityyakyak@outlook.com>
2022-06-30 14:26:11 +0200

    basic sheduler

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/vlkrrrr/cplogin
 
 go 1.18
+
+require github.com/knadh/koanf v1.4.2
+
+require (
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	golang.org/x/sys v0.0.0-20200331124033-c3d80250170d // indirect
+	gopkg.in/yaml.v3 v3.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+)
+
+/*
+read login, password, url, sleeptime after success from file
+create go routine that fires login request and sleeps after success
+*/
+var config = koanf.New(":")
 
 func main() {
-	fmt.Println("test")
+	config.Load(file.Provider("config.yml"), yaml.Parser())
+	go login(config.String("url"), config.String("login:user"), config.String("login:password"), time.Duration(config.Int("timeout")))
+}
+
+func login(url string, user string, password string, duration time.Duration) {
+	client := http.Client{}
+	loginreq, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Printf("error while creating request: %v", err)
+	}
+	loginreq.SetBasicAuth(user, password)
+	res, err := client.Get(url)
+	if err != nil {
+		fmt.Printf("error while sending simple request %v", err)
+	}
+	retry := 0
+	if res.StatusCode == 200 {
+		retry = 0
+		time.Sleep(duration * time.Minute)
+	} else {
+		client.Do(loginreq)
+		retry++
+		time.Sleep(time.Duration(retry) * time.Minute)
+	}
+
+	fmt.Printf("client: status code: %d\n", res.StatusCode)
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("client: could not read response body: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("client: response body: %s\n", resBody)
 }
