dd148af2d9b62c1423237ba8ce3fea7bc8537096
vlkrrrr <yakityyakyak@outlook.com>
2022-06-30 14:57:18 +0200

    added logger

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -17,9 +18,18 @@ read login, password, url, sleeptime after success from file
 create go routine that fires login request and sleeps after success
 */
 var config = koanf.New(":")
+var logger = &log.Logger{}
 
 func main() {
-	config.Load(file.Provider("config.yml"), yaml.Parser())
+	logfile, err := os.Create("cp_log.txt")
+	if err != nil {
+		fmt.Printf("Could not create/append file: %v", err)
+	}
+	logger = log.New(logfile, "CP_", log.LstdFlags)
+	err = config.Load(file.Provider("config.yml"), yaml.Parser())
+	if err != nil {
+		logger.Fatalf("read config error; %v", err)
+	}
 	go login(config.String("url"), config.String("login:user"), config.String("login:password"), time.Duration(config.Int("timeout")))
 }
 
@@ -27,12 +37,12 @@ func login(url string, user string, password string, duration time.Duration) {
 	client := http.Client{}
 	loginreq, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		fmt.Printf("error while creating request: %v", err)
+		logger.Printf("error while creating request: %v", err)
 	}
 	loginreq.SetBasicAuth(user, password)
 	res, err := client.Get(url)
 	if err != nil {
-		fmt.Printf("error while sending simple request %v", err)
+		logger.Printf("error while sending simple request %v", err)
 	}
 	retry := 0
 	if res.StatusCode == 200 {
@@ -44,11 +54,10 @@ func login(url string, user string, password string, duration time.Duration) {
 		time.Sleep(time.Duration(retry) * time.Minute)
 	}
 
-	fmt.Printf("client: status code: %d\n", res.StatusCode)
+	logger.Printf("client: status code: %d\n", res.StatusCode)
 	resBody, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		fmt.Printf("client: could not read response body: %s\n", err)
-		os.Exit(1)
+		logger.Printf("client: could not read response body: %s\n", err)
 	}
-	fmt.Printf("client: response body: %s\n", resBody)
+	logger.Printf("client: response body: %s\n", resBody)
 }
